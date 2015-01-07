8e66b0fd65764684294e46ea24fadede1b3c6bbb
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-01-07 18:20:47 +0200

    Added livereload

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = module.exports.app = exports.app = express();
+app.use(require('connect-livereload')());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
