f8ee89fd1e0ada4508f064c5dfe6c8ee09bf594e
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-01-28 00:05:07 +0000

    Added parsers for multipart requests
    When dealing with multipart requests we want to handle the request
    differently. Connect-multiparty is an for multiparty on express.

--- app/controllers/editProfile.js
+++ app/controllers/editProfile.js
@@ -3,9 +3,18 @@
 
 var ox = angular.module('oxstututors');
 
-ox.controller('EditProfileCtrl', function($scope, $location, User){
+ox.controller('EditProfileCtrl', function($scope, $location, User, FileUploader){
     if(!$scope.currentUser) {
         $location.path('/login');
-    }
+    };
 
+    $scope.saveChanges = function() {
+        console.log('Saving');
+        User.update();
+    };
+
+    $scope.uploader = new FileUploader({
+        url: 'auth/users/profileImage',
+        queueLimit: 1
+    });
 });
