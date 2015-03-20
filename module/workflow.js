236908bdaaac9bb964333407466eab24a49265d5
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-20 19:53:41 +0200

    [Reformat;Code] Added status to workflow
    Reformatted and added the status in the header.

--- module/workflow.js
+++ module/workflow.js
@@ -1,26 +1,32 @@
 'use strict';
 
-module.exports = function(req, res) {
-    var workflow = new (require('events').EventEmitter)();
+module.exports = function (req, res) {
+  var workflow = new (require('events').EventEmitter)();
 
-    workflow.outcome = {
-        success: false,
-        errors: []
-    };
+  workflow.outcome = {
+    success: false,
+    errors: []
+  };
 
-    workflow.hasErrors = function() {
-        return workflow.outcome.errors.length !== 0;
-    };
+  workflow.hasErrors = function () {
+    return workflow.outcome.errors.length !== 0;
+  };
 
-    workflow.on('exception', function(err) {
-        workflow.outcome.errors.push('Exception: '+ err);
-        return workflow.emit('response');
-    });
+  workflow.on('exception', function (err) {
+    workflow.outcome.errors.push('Exception: ' + err);
+    return workflow.emit('response');
+  });
 
-    workflow.on('response', function() {
-        workflow.outcome.success = !workflow.hasErrors();
-        res.send(workflow.outcome);
-    });
+  workflow.on('response', function () {
+    workflow.outcome.success = !workflow.hasErrors();
 
-    return workflow;
+    var statusCode = 200;
+    if(!workflow.outcome.success) {
+      statusCode = 404;
+    }
+
+    res.status(statusCode).send(workflow.outcome);
+  });
+
+  return workflow;
 };
