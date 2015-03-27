2a660d7aba3149a2e66c2b955e143c1a117ca59e
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-27 12:04:36 +0200

    [Code] Prepared User resource for search

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,5 +1,5 @@
 var ox = angular.module('oxstututors');
 
-ox.controller('SearchCtrl', ['$scope', function($scope) {
-  
+ox.controller('SearchCtrl', ['$scope', 'User', function($scope, User) {
+  $scope.search = User.search();
 }]);
