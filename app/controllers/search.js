971a711a97661897c8e9ed136d7697bf470d69af
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-27 23:43:28 +0200

    [Code] Search page update

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,5 +1,55 @@
 var ox = angular.module('oxstututors');
 
-ox.controller('SearchCtrl', ['$scope', 'User', function($scope, User) {
-  $scope.search = User.search();
+ox.controller('SearchCtrl', ['$scope', 'User', 'Subject', 'Profile',
+  function($scope, User, Subject, Profile) {
+    $scope.categories = [
+      'Personal Statement',
+      'Oxbridge Admissions Test',
+      'Oxbridge Interview',
+      'A-Levels',
+      'IB',
+      'SAT II',
+      'GCSE'];
+
+    $scope.search = [];
+    $scope.search = User.search();
+    $scope.selectedCategories = {};
+
+    $scope.selectAllCategories = function (value) {
+      for(var i in $scope.categories) {
+        $scope.selectedCategories[$scope.categories[i]] = value;
+      }
+    };
+
+    var prettySubject = Profile.subjectName;
+    var subjects = [];
+
+    Subject.get({}, function(sbjs){
+      console.log(sbjs);
+      for(var i = 0; i < sbjs.length; i++) {
+        subjects[i] = prettySubject(sbjs[i].subject);
+      }
+      console.log(subjects);
+    });
+
+    $scope.getSubjects = function(query) {
+      var filter = createFilterFor(query);
+      var results = [];
+
+      for(var i = 0; i < subjects.length; i++) {
+        if(filter(subjects[i])){
+          results.push(subjects[i]);
+        }
+      }
+
+      return results;
+    };
+
+    function createFilterFor(query) {
+      return function(item) {
+        var lowercaseQuery = angular.lowercase(query);
+        var lowercaseSubject = angular.lowercase(item);
+        return lowercaseSubject.indexOf(lowercaseQuery) === 0;
+      }
+    }
 }]);
