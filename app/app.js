396dc667d0bf9265e5d1ee7926f99843b4fd511a
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-27 10:25:25 +0200

    [Refactor] app.js

--- app/app.js
+++ app/app.js
@@ -1,46 +1,46 @@
 'use strict';
 
 angular.module('oxstututors', [
-    'ui.bootstrap',
-    'ngRoute',
-    'ngResource',
-    'http-auth-interceptor',
-    'ngMaterial',
-    'angularFileUpload'
+  'ui.bootstrap',
+  'ngRoute',
+  'ngResource',
+  'http-auth-interceptor',
+  'ngMaterial',
+  'angularFileUpload'
 ])
-    .config(function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: '/index',
-                controller: 'MainCtrl'
-            })
-            .when('/signup/', {
-                templateUrl: '/auth/signup',
-                controller: 'SignUpCtrl'
-            })
-            .when('/login/', {
-                templateUrl: '/auth/login',
-                controller: 'LoginCtrl'
-            })
-            .when('/users/:userId/', {
-                templateUrl: '/users/',
-                controller: 'UsersCtrl'
-            })
-            .when('/editProfile/', {
-                templateUrl: '/users/edit',
-                controller: 'EditProfileCtrl'
-            })
-            .otherwise({
-                // Should redirect to an error page
-                redirectTo: '/'
-            });
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/index',
+        controller: 'MainCtrl'
+      })
+      .when('/signup/', {
+        templateUrl: '/auth/signup',
+        controller: 'SignUpCtrl'
+      })
+      .when('/login/', {
+        templateUrl: '/auth/login',
+        controller: 'LoginCtrl'
+      })
+      .when('/users/:userId/', {
+        templateUrl: '/users/',
+        controller: 'UsersCtrl'
+      })
+      .when('/editProfile/', {
+        templateUrl: '/users/edit',
+        controller: 'EditProfileCtrl'
+      })
+      .otherwise({
+        // Should redirect to an error page
+        redirectTo: '/'
+      });
 
-        $locationProvider.html5Mode(true);
-    })
-    .run(function ($rootScope, $location) {
-        // On catching 401 errors, redirect to the login page.
-        $rootScope.$on('event:auth-loginRequired', function() {
-            $location.path('/login');
-            return false;
-        });
+    $locationProvider.html5Mode(true);
+  })
+  .run(function ($rootScope, $location) {
+    // On catching 401 errors, redirect to the login page.
+    $rootScope.$on('event:auth-loginRequired', function () {
+      $location.path('/login');
+      return false;
     });
+  });
