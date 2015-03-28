8d38bb456bf67a97fbd343f99fb342272166d2a0
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-28 20:05:39 +0200

    [Code] Filtering users in inbox new message.

--- app/controllers/inbox.js
+++ app/controllers/inbox.js
@@ -3,6 +3,25 @@
 
 var ox = angular.module('oxstututors');
 
-ox.controller('InboxCtrl', ['$scope', function($scope) {
+ox.controller('InboxCtrl', ['$scope', 'Message', 'User', function($scope, Message, User) {
+  $scope.userBase = User.allUsers();
+
   $scope.test = "Andrei";
+
+  $scope.filterUserBase = function() {
+    return $scope.userBase.filter(function(user) {
+      var lowercaseUserName = angular.lowercase(user.username);
+      var lowercaseSearchText = angular.lowercase($scope.searchText);
+      return (lowercaseUserName && lowercaseUserName.indexOf(lowercaseSearchText) == 0);
+    })
+  };
+
+  $scope.sendMessage = function() {
+    var message = {
+      from: $scope.currentUser,
+      to: $scope.receiver,
+      message: $scope.message
+    };
+    Message.post(message);
+  }
 }]);
