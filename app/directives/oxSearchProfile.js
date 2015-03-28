2821b639176031d4ebc7c017b472dbc10aed0763
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-28 11:26:44 +0200

    [Code] Search filters

--- app/directives/oxSearchProfile.js
+++ app/directives/oxSearchProfile.js
@@ -5,10 +5,63 @@ ox.directive('oxSearchProfile', function() {
     restrict: 'E',
     templateUrl: '/pages/oxSearchProfile',
     scope: {
-      users: '='
+      users: '=',
+      selectedSubject: '=',
+      selectedCategories: '='
     },
     controller : function($scope, Profile) {
       $scope.prettySubject = Profile.subjectName;
     }
+  }
+});
+
+ox.filter('filterSubject', ['Profile', function(Profile) {
+  return function(input, subject, categories) {
+    if(!subject) {
+      return input;
+    }
+
+    var output = [];
+
+    for(var i = 0; i < input.length; i++) {
+      var sbjs = input[i].profile.subjects;
+      for(var j = 0; j < sbjs.length; j++) {
+        if(Profile.subjectName(sbjs[j].subject) === subject) {
+          output.push(input[i]);
+        }
+      }
+    }
+
+    return output;
+  }
+}]);
+
+ox.filter('filterCategories', function() {
+  return function(input, categories) {
+    if(!categories) {
+      return input;
+    }
+
+    var output = [];
+
+    for(var i = 0; i < input.length; i++) {
+      var sbjs = input[i].profile.subjects;
+
+      var toAdd = true;
+
+      for(var j = 0; j < sbjs.length; j++){
+        for(var key in categories) {
+          if(categories[key] && !sbjs[j].categories[key]) {
+            toAdd = false;
+          }
+        }
+      }
+
+      if(toAdd) {
+        output.push(input[i]);
+      }
+    }
+
+    return output;
   }
 });
